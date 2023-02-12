b1ddf102385a0d1dc39e2e965deb1f052b0f2101
onion <wsrfssx@yeah.net>
2023-02-12 23:47:33 +0800

    doc

--- Framework/Interface/IRuntimeModule.hpp
+++ Framework/Interface/IRuntimeModule.hpp
@@ -2,15 +2,15 @@
 #include "Interface.hpp"
 
 // 运行模块
-namespace My{
+namespace My {
     Interface IRuntimeModule{
         public:
-            virtual ~IRuntimeModule(){}
+            virtual ~IRuntimeModule() {};
             // 初始化模块
             virtual int Initialize() = 0;
             // 模块结束，打扫战场用
             virtual void Finalize() = 0;
             // 模块执行，每次进行一个单位的处理
             virtual void Tick() = 0;
-    }
+    };
 }
