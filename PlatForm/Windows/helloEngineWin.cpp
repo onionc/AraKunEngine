310bbeee6a1d60b7c50967eb51e88b20531e1c94
chad <wsrfssx@yeah.net>
2023-02-27 23:38:56 +0800

    fix: 小球。移动正在进行

--- PlatForm/Windows/helloEngineWin.cpp
+++ PlatForm/Windows/helloEngineWin.cpp
@@ -1,14 +1,16 @@
 #include <windows.h>
 #include <tchar.h>
-#include <new>
 
-#undef UNICODE
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
-typedef struct StateInfo{
-    char a;
-}StateInfo;
+
+// 接球板
+static const int width = 50, height = 10, start=0;
+static RECT board = {start, 300, start+width, 300+height};
+// 小球
+static const int ballSize = 10;
+static RECT ball = {start+width/2-ballSize/2, board.top - ballSize, ball.left+ballSize, board.top};
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
     const TCHAR CLASS_NAME[] = _T("WindowClass1"); 
@@ -25,53 +27,77 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     RegisterClassEx(&wc);
 
-    StateInfo *pState = new (std::nothrow) StateInfo;
-    HWND hwnd = CreateWindowEx(0, CLASS_NAME, _T("Hello Engine"),
+    HWND hwnd = CreateWindowEx(0, CLASS_NAME, _T("ping"),
         WS_OVERLAPPEDWINDOW,
         300, 300, 500, 400,
-        NULL, NULL, hInstance, pState);
+        NULL, NULL, hInstance, NULL);
 
     if(hwnd == NULL) return 0;
 
     ShowWindow(hwnd, nCmdShow);
 
-    MSG msg;
 
+    SetTimer (hwnd, 1, 1000, NULL) ;
+    
+    MSG msg;
     while(GetMessage(&msg, NULL, 0, 0)){
         TranslateMessage(&msg);
 
         DispatchMessage(&msg);
     }
 
+    
+
+
     return msg.wParam;
 }
 
-StateInfo* GetAppState(HWND hwnd){
-    LONG_PTR ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
-    return (StateInfo*)ptr;
-}
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
-    // 获取对话框对象
-    StateInfo *pState;
-    if(message == WM_CREATE){
-        CREATESTRUCT *pCreate = (CREATESTRUCT*)lParam;
-        pState = (StateInfo*)(pCreate->lpCreateParams);
-        SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)pState);
-    }else{
-        pState = GetAppState(hWnd);
+
+// 重力
+float g = 9.8;
+// 弹力
+float f = 0;
+// 弹力方向
+float fpoint = 90;
+
+// 系数
+float k=0.1;
+
+// 移动球
+void move(){
+    if(fpoint>1e-4 && fpoint-1e-4<90){
+        // 0~90
+
     }
-    // 接球板
-    static const int width = 50, height = 10, start=0;
-    static RECT board = {start, 300, start+width, 300+height};
+
+
+    // 碰撞检测
+
+    // 弹力衰减
+    if(f>5){
+        f-=5;
+    }
+    if(fpoint>2){
+        fpoint-=2;
+    }
+
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
+
     switch(message){
         case WM_PAINT:
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             HBRUSH brush = (HBRUSH) GetStockObject(BLACK_BRUSH);
-
-
             FillRect(hdc, &board, brush);
+
+            // 画圆
+            SelectObject(hdc, brush);
+            Ellipse(hdc, ball.left, ball.top, ball.right, ball.bottom);
+            DeleteObject(brush);
+
             EndPaint(hWnd, &ps);
         }
             break;
@@ -80,6 +106,7 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
                 case VK_LEFT:
                     if(board.left>=10){
                         board.left-=10;
+                        ball.left-=10;
                         board.right = board.left+width;
                         InvalidateRect(hWnd, NULL, TRUE);
                     }
@@ -100,6 +127,10 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
+            break;
+        case WM_TIMER:
+            ball.right++;
+            InvalidateRect(hWnd, NULL, FALSE);
             break;
     }
 
