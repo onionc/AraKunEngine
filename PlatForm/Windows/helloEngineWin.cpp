c8f7e33f901493724d4f8a6e3ead97d180eaf853
onion <wsrfssx@yeah.net>
2023-03-03 23:09:17 +0800

    fix: 墙壁碰撞

--- PlatForm/Windows/helloEngineWin.cpp
+++ PlatForm/Windows/helloEngineWin.cpp
@@ -2,6 +2,16 @@
 #include <tchar.h>
 #include <string>
 #include <iostream>
+#include <fstream>
+
+
+// log
+std::ofstream wf;
+
+// 创建时指定的窗口大小
+#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 400
+RECT clientRect; // 真实客户区大小
 
 // 向量
 typedef struct VecS{
@@ -42,7 +52,7 @@ static RECT ball = {start+width/2-ballR, board.top - ballSize, ball.left+ballSiz
 
 
 // 重力
-Vec g{0,-1};
+Vec g{0,-3};
 // 弹力
 Vec bounce;
 // 摩擦力
@@ -73,15 +83,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     HWND hwnd = CreateWindowEx(0, CLASS_NAME, _T("ping"),
         WS_OVERLAPPEDWINDOW,
-        300, 300, 500, 400,
+        300, 300, WINDOW_WIDTH, WINDOW_HEIGHT,
         NULL, NULL, hInstance, NULL);
 
     if(hwnd == NULL) return 0;
 
     ShowWindow(hwnd, nCmdShow);
 
+    // log
+    wf.open("run.log", std::ios::out | std::ios::trunc );
 
-    SetTimer (hwnd, 1, 50, NULL) ;
+    // 获取客户区大小
+    GetClientRect(hwnd, &clientRect);
+    wf<<"client size:"<<clientRect.right<<","<<clientRect.bottom;
+
+    // 定时器
+    SetTimer (hwnd, 1, 100, NULL) ;
     
     MSG msg;
     while(GetMessage(&msg, NULL, 0, 0)){
@@ -100,26 +117,46 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 // 移动球
 void move(){
-    
+    RECT ballOld = ball;
+
+    wf << "bounce="<<bounce.toString()<<std::endl;
     // 球移动（在重力和弹力的作用下）
     Vec t{g.x+bounce.x, g.y+bounce.y};
 
     ballUpdate(t);
-    // 碰撞检测
+    // 板和球 碰撞检测
     if(hitDetection(ball, board)){
+        // 碰撞，首先撤回
+        ball = ballOld; // todo: 应该贴近，直接撤回会有卡顿
+
         // 弹力为两个中心连线向量，方向从board到ball, 大小随机
         float x = (ball.right+ball.left)/2.0 - (board.right+board.left)/2.0;
         float y = (ball.bottom+ball.top)/2.0 - (board.bottom+board.top)/2.0;
 
-        float k2 =1;
+        float k2 =0.1;
         
- 
-        bounce.x = x*k2;
-        bounce.y = y*k2;
+        wf << "x="<<x<<",y="<<y<<std::endl;
+        bounce.x += x*k2;
+        bounce.y += y*k2;
     }
+
+    // 墙壁碰撞
+    if(ball.left<1e-5 || ball.right>clientRect.right){
+        bounce.x*=-1;
+        ball = ballOld;
+    }else if(ball.top<1e-5 || ball.bottom>clientRect.bottom){
+        bounce.y*=-1;
+        bounce.y+=g.y;
+        ball = ballOld;
+    }
+
+
     // 弹力衰减
-    if(bounce.x>1) bounce.x-=1;
-    if(bounce.y>1) bounce.y-=1;
+    //if(bounce.x>0.3) bounce.x-=0.15;
+    //if(bounce.x<-0.3) bounce.x+=0.15;
+    //if(bounce.y>0.3) bounce.y-=0.15;
+    //if(bounce.y<-0.3) bounce.y+=0.15;
+
 }
 // 碰撞检测
 bool hitDetection(RECT &rect1, RECT &rect2){
@@ -149,6 +186,9 @@ void ballUpdate(Vec &pos){
     ball.top = pos2.y-ballR;
     ball.bottom = pos2.y+ballR;
 
+    wf << "start:" << start.toString() << std::endl;
+    wf << "+:" << pos.toString() << std::endl;
+    wf << "ball:" << pos2.toString() << std::endl;
 }
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
